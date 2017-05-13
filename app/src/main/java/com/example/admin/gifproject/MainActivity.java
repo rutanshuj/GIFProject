776061959cf1765ef4d350a8bb6d459ecce98c19
rutanshuj <rjhaveri41@gmail.com>
2017-05-13 16:51:48 +0530

    Storage Changes

--- app/src/main/java/com/example/admin/gifproject/MainActivity.java
+++ app/src/main/java/com/example/admin/gifproject/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.admin.gifproject;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -18,6 +24,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
@@ -30,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         b1 = (Button) findViewById(R.id.capture);
 
         imageView = (ImageView) findViewById(R.id.picview);
@@ -38,48 +48,80 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i, CAMERA_REQUEST);
+//
+//                Intent cameraIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+//
+////                  File picDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+////                String picName = getPictureName();
+////                File imageFile = new File(picDirectory, picName);
+////                Uri uri = Uri.fromFile(imageFile);
+////                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+//
+//                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+
 
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 AnimatedGifEncoder encoder = new AnimatedGifEncoder();
-                encoder.setDelay(500);  // ディレイ 500/ms
-                encoder.setRepeat(0);   // 0:ループする -1:ループしない
-                encoder.start(bos);     // gitデータ生成先ををbosに設定
+                encoder.setDelay(500);
+                encoder.setRepeat(0);
+                encoder.start(bos);
 
                 try {
                     Bitmap bmp1, bmp2, bmp3;
 
-                    // ファイルの読み込み
-                    bmp1 = BitmapFactory.decodeStream(new FileInputStream("/sdcard/target1.png"));
-                    encoder.addFrame(bmp1);  // gifに追加
-                    bmp1.recycle();
 
-                    bmp2 = BitmapFactory.decodeStream(new FileInputStream("/sdcard/target2.png"));
-                    encoder.addFrame(bmp2);  // gifに追加
-                    bmp2.recycle();
+                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                            R.mipmap.ic_launcher);
+                    encoder.addFrame(bitmap);
+                    bitmap.recycle();
 
-                    bmp3= BitmapFactory.decodeStream(new FileInputStream("/sdcard/target3.png"));
-                    encoder.addFrame(bmp3);  // gifに追加
-                    bmp3.recycle();
 
-                } catch (FileNotFoundException e) {
+                    Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(),
+                            R.drawable.images);
+                    encoder.addFrame(bitmap1);
+                    bitmap.recycle();
+
+                    Bitmap bitmap2 = BitmapFactory.decodeResource(getResources(),
+                            R.drawable.mm);
+                    encoder.addFrame(bitmap2);
+
+
+
+                } catch (Exception e) {
                 }
 
-                encoder.finish();  // 終了
+                encoder.finish();
 
-                File filePath = new File("/sdcard", "sample.gif");
-                FileOutputStream outputStream;
+                String fileName = "note.gif";
+
+                File extStore = Environment.getExternalStorageDirectory();
+                // ==> /storage/emulated/0/note.txt
+                String path = extStore.getAbsolutePath() + "/" + fileName;
+                Log.i("ExternalStorageDemo", "Save to: " + path);
+
+
                 try {
-                    outputStream = new FileOutputStream(filePath);
-                    // bosに生成されたgifデータをファイルに吐き出す
-                    outputStream.write(bos.toByteArray());
-                } catch (FileNotFoundException e) {
-                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, "Lol", Toast.LENGTH_SHORT).show();
+                    File myFile = new File(path);
+                    myFile.createNewFile();
+                    FileOutputStream fOut = new FileOutputStream(myFile);
+
+                    fOut.write(bos.toByteArray());
+                    fOut.close();
+                    Toast.makeText(getApplicationContext(), fileName + " saved", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
 
+    }
+
+    private String getPictureName() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String timestamp = sdf.format(new Date());
+        return "gifImage" + timestamp + ".jpg";
     }
 
     @Override
@@ -88,11 +130,11 @@ public class MainActivity extends AppCompatActivity {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
 
-            showDialog();
+            showDialog(photo);
         }
     }
 
-    private void showDialog() {
+    private void showDialog(Bitmap photo) {
         final Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.customdialog);
         dialog.setTitle("What's next");
@@ -108,7 +150,6 @@ public class MainActivity extends AppCompatActivity {
         device.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
 
